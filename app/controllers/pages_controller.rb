b1027a89f91d476ed6932d42bf50ef550ca14553
William Poirier <wpoirier92@gmail.com>
2017-07-23 16:53:25 +0000

    Finishing Task 3

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,12 @@
 class PagesController < ApplicationController
   def home
   end
-
   def sms
-    #byebug
     #raise "Action sms in the UsersController is not implemented! received params #{params.to_json}"
+    #byebug
+    puts params["phone"];
+    puts params["text"];
+    @phone = params["phone"];
+    @text = params["text"];
   end
 end
