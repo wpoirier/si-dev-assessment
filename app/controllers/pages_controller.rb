31666bffeba7d07f1db05a6b108fe58ca891c62c
William Poirier <wpoirier92@gmail.com>
2017-07-23 18:02:11 +0000

    Finishing Task 6 - initial version

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,5 @@
+require 'twilio-ruby'
+
 class PagesController < ApplicationController
   def home
   end
@@ -8,6 +10,14 @@ class PagesController < ApplicationController
     puts params["text"];
     @phone = params["phone"];
     @text = params["text"];
-    
+    account_sid = "ACb6b3ceafd02250aab730da69d3788942";
+    auth_token = "75135711a30a39ae058b84a341162be9";
+    twilio_no = "+15087147386";
+    @client = Twilio::REST::Client.new account_sid, auth_token
+    message = @client.messages.create(
+      body: @text,
+      to: @phone,    # Replace with your phone number
+      from: twilio_no)  # Replace with your Twilio number
+    puts message.sid
   end
 end
