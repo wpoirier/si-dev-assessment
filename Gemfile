80b7120182a3fd11ed2de22699bfcd5a50516a43
William Poirier <wpoirier92@gmail.com>
2017-07-24 03:50:46 +0000

    Finishing Task 6

--- Gemfile
+++ Gemfile
@@ -55,3 +55,5 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 ruby "2.3.0"
+
+gem 'figaro'
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,20 +4,25 @@ class PagesController < ApplicationController
   def home
   end
   def sms
+    @test = account_sid = ENV["SID"];
     #raise "Action sms in the UsersController is not implemented! received params #{params.to_json}"
     #byebug
-    puts params["phone"];
-    puts params["text"];
-    @phone = params["phone"];
-    @text = params["text"];
-    account_sid = "ACb6b3ceafd02250aab730da69d3788942";
-    auth_token = "75135711a30a39ae058b84a341162be9";
-    twilio_no = "+15087147386";
+    #puts params["phone"];
+    #puts params["text"];
+    @phone = params["phone"].html_safe;
+    @text = params["text"].html_safe;
+    #account_sid = "ACb6b3ceafd02250aab730da69d3788942";
+    #auth_token="75135711a30a39ae058b84a341162be9";
+    #twilio_no="+15087147386";
+    account_sid = ENV["SID"];
+    auth_token = ENV["TOKEN"];
+    twilio_no = ENV["TWILIO"];
+    
     @client = Twilio::REST::Client.new account_sid, auth_token
     message = @client.messages.create(
       body: @text,
-      to: @phone,    # Replace with your phone number
-      from: twilio_no)  # Replace with your Twilio number
+      to: @phone,
+      from: twilio_no)
     puts message.sid
   end
 end
